669e9938803d14350ab0d4db8459838bf0a26e1f
eder hernandez <eder.hernandez@telvent.com>
2015-07-08 15:19:30 +0200

    Third submit of first assignment

--- fpps/recfun/src/main/scala/recfun/Main.scala
+++ fpps/recfun/src/main/scala/recfun/Main.scala
@@ -2,14 +2,6 @@ package recfun
 import common._
 
 object Main {
-  def main(args: Array[String]) {
-    println("Pascal's Triangle")
-    for (row <- 0 to 10) {
-      for (col <- 0 to row)
-        print(pascal(col, row) + " ")
-      println()
-    }
-  }
 
   /**
    * Exercise 1
